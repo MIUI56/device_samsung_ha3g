exynos5420/android_device_samsung_n1awifi

n1awifi: initial device tree
Change-Id: Ie96634d5524f4b0a7673d1f8fb0e0f14c7a69949

lineage_n1awifi.mk
@@ -1,0 +1,40 @@
+#
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from n1awifi device
+$(call inherit-product, device/samsung/n1awifi/device.mk)
+
+# Inherit some common lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := n1awifi
+PRODUCT_NAME := lineage_n1awifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-P600
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=n1awifi \
+    PRIVATE_BUILD_DESC="lt03wifiue 5.1.1 LMY47X P600UEUDOK1 release-keys"
+
+BUILD_FINGERPRINT := samsung/lt03wifiue/lt03wifiue:5.1.1/LMY47X/P600UEUDOK1:user/release-keys

MIUI56/device_samsung_ha3g

lineage_n1awifi.mk
@@ -1,0 +1,40 @@
+#
+# Copyright (C) 2014-2016 The CyanogenMod Project
+# Copyright (C) 2017-2018 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+# Inherit from n1awifi device
+$(call inherit-product, device/samsung/n1awifi/device.mk)
+
+# Inherit some common lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := n1awifi
+PRODUCT_NAME := lineage_n1awifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-P600
+PRODUCT_MANUFACTURER := samsung
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=n1awifi \
+    PRIVATE_BUILD_DESC="lt03wifiue 5.1.1 LMY47X P600XXUDQA1 release-keys"
+
+BUILD_FINGERPRINT := samsung/lt03wifiue/lt03wifiue:5.1.1/LMY47X/P600XXUDQA1:user/release-keys